5443301a99cb1c66973a9a98e287c7764eb60c9c
sderohan <rohanshinde7212@gmail.com>
2022-01-11 20:20:53 +0530

    add comments

--- controller/controller.go
+++ controller/controller.go
@@ -10,20 +10,25 @@ import (
 
 func GetTopTenWords(w http.ResponseWriter, r *http.Request) {
 	req := new(payload.TopWordRequest)
+	// decode the request and get the data
 	if err := payload.DecodeReq(r, req); err != nil {
 		payload.JSON(w, 400, err)
 		return
 	}
 
+	// remove spaces and check for empty
 	req.Text = strings.Trim(req.Text, " ")
 	if req.Text == "" {
 		payload.JSON(w, 400, "Request body is empty!")
 	}
 
+	// call the service for performing word count
 	data, err := service.GetTopTenWords(req.Text)
 	if err != nil {
 		payload.JSON(w, 400, err)
 		return
 	}
+
+	// return the word counts
 	payload.JSON(w, 200, data)
 }
